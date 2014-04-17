0f605ec0167c2c17b579d3d012f1ecf2622cb0a3
Filip Lindqvist <buggfille@gmail.com>
2014-04-17 12:20:39 +0200

    * Added kyroNet

--- server/src/main/java/com/wonkytonki/server/Main.java
+++ server/src/main/java/com/wonkytonki/server/Main.java
@@ -1,7 +1,14 @@
 package com.wonkytonki.server;
 
+import com.esotericsoftware.kryonet.Server;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world");
+        Server server = new Server();
+        server.start();
+        server.bind(54555, 54777);
     }
 }
