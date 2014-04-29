394019989b1ae1f20f1f3dd456a45183d159e7af
Filip Lindqvist <ada10fl2@student.lu.se>
2014-04-29 11:26:47 +0200

    Initial Server listnening to 1337

--- server/src/main/java/com/wonkytonki/server/Main.java
+++ server/src/main/java/com/wonkytonki/server/Main.java
@@ -1,14 +1,22 @@
 package com.wonkytonki.server;
 
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
-
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello world");
+        System.out.println("Initializing...");
         Server server = new Server();
         server.start();
-        server.bind(54555, 54777);
+        server.bind(1337, 1338);
+        
+        System.out.println("Running...");
+        server.addListener(new Listener() {
+            public void received (Connection connection, Object object) {
+                connection.sendTCP(connection.getRemoteAddressTCP().toString());
+            }
+        });
     }
 }
