be2e16268147860f0587353a4e5c7c86f2c013ce
Filip Lindqvist <ada10fl2@student.lu.se>
2014-04-29 16:46:41 +0200

    Implemented server

---
+++ app/src/main/java/com/wonkytonki/app/AudioFrame.java
@@ -0,0 +1,9 @@
+package com.wonkytonki.app;
+
+/**
+ * Created by Filip on 2014-04-29.
+ */
+public class AudioFrame {
+    public long time;
+    public byte[] bytes;
+}
--- server/src/main/java/com/wonkytonki/server/Main.java
+++ server/src/main/java/com/wonkytonki/server/Main.java
@@ -2,6 +2,7 @@ package com.wonkytonki.server;
 
 import com.esotericsoftware.kryonet.*;
 import com.esotericsoftware.kryo.*;
+import com.wonkytonki.server.messages.AudioFrame;
 
 import java.io.*;
 import java.util.*;
@@ -9,15 +10,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Initializing...");
-        Server server = new Server();
+
+        Server server = new Server(1024 * 1024, 1024 * 1024);
         server.start();
         server.bind(54555, 54777);
-        
+
         Kryo k = server.getKryo();
-        //k.register(byte[].class);
-                
+        k.setRegistrationRequired(false);
+        k.register(AudioFrame.class);
+        
         System.out.println("Running...");
         final List<Connection> pool = Collections.synchronizedList(new ArrayList<Connection>());
+
         server.addListener(new Listener() {
             @Override
             public void connected(Connection connection) {
@@ -28,9 +32,20 @@ public class Main {
             
             @Override
             public void received (Connection connection, Object object) {
-                System.out.println("Message from: "+connection.getRemoteAddressTCP());
-                if(object instanceof String) {
-                    System.out.println("Data: "+ object);
+                System.out.println(".");
+                if(object instanceof AudioFrame) {
+                    AudioFrame af = (AudioFrame) object;
+                    if ( af.time > System.currentTimeMillis() - 1000) {
+                        for (Connection peer : pool) {
+                            if (peer.getRemoteAddressTCP().equals(connection.getRemoteAddressTCP())) continue;
+                            System.out.println("Sending reply to "+peer.getRemoteAddressTCP());
+                            peer.sendTCP(object);
+                        }
+                    } else {
+                        System.out.println("Discarding old audio frame: "+af.time);
+                    }
+                }
+                if(object instanceof byte[]){
                     for (Connection peer : pool) {
                         if (peer.getRemoteAddressTCP().equals(connection.getRemoteAddressTCP())) continue;
                         System.out.println("Sending reply to "+peer.getRemoteAddressTCP());
