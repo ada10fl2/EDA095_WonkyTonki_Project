d55d2d7d734fa51671d72e9008748d66393ade6f
Filip Lindqvist <ada10fl2@student.lu.se>
2014-04-29 13:20:16 +0200

    Added kyronet new version , implemented server recdirect of data

--- server/src/main/java/com/wonkytonki/server/Main.java
+++ server/src/main/java/com/wonkytonki/server/Main.java
@@ -1,21 +1,49 @@
 package com.wonkytonki.server;
 
-import com.esotericsoftware.kryonet.Connection;
-import com.esotericsoftware.kryonet.Listener;
-import com.esotericsoftware.kryonet.Server;
-import java.io.IOException;
+import com.esotericsoftware.kryonet.*;
+import com.esotericsoftware.kryo.*;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Initializing...");
         Server server = new Server();
         server.start();
-        server.bind(1337, 1338);
+        server.bind(54555, 54777);
         
+        Kryo k = server.getKryo();
+        //k.register(byte[].class);
+                
         System.out.println("Running...");
+        final List<Connection> pool = Collections.synchronizedList(new ArrayList<Connection>());
         server.addListener(new Listener() {
+            @Override
+            public void connected(Connection connection) {
+                System.out.println("Connected: "+connection.getRemoteAddressTCP().getHostString());
+                pool.add(connection);
+                System.out.println("Clients: "+pool.size()+"");
+            }
+            
+            @Override
             public void received (Connection connection, Object object) {
-                connection.sendTCP(connection.getRemoteAddressTCP().toString());
+                System.out.println("Message from: "+connection.getRemoteAddressTCP());
+                if(object instanceof String) {
+                    System.out.println("Data: "+ object);
+                    for (Connection peer : pool) {
+                        if (peer.getRemoteAddressTCP().equals(connection.getRemoteAddressTCP())) continue;
+                        System.out.println("Sending reply to "+peer.getRemoteAddressTCP());
+                        peer.sendTCP(object);
+                    }
+                }
+            }
+
+            @Override
+            public void disconnected(Connection connection) {
+                System.out.println("Disconnected: "+connection.toString());
+                pool.remove(connection);
+                System.out.println("Clients: "+pool.size()+"");
             }
         });
     }
